0e5eab39f3c8ace8c788104c8e95f2d28d39c088
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2022-02-18 01:56:56 +0000

    all done with theme colors an dgraphql initial setup

--- lib/pages/auth/authentication_page.dart
+++ lib/pages/auth/authentication_page.dart
@@ -1,5 +1,6 @@
 import 'package:dynasty_urban_style/index.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AuthenticationPage extends StatefulWidget {
@@ -11,19 +12,11 @@ class AuthenticationPage extends StatefulWidget {
 
 class _AuthenticationPageState extends State<AuthenticationPage> {
   // state colors
-  Color _signinBackgroundColor = BrandColors.kColorDarkBlue;
-  Color _signupBackgroundColor = BrandColors.kWhiteGray;
-  Color _signinTextColor = BrandColors.kWhite;
-  Color _signupTextColor = BrandColors.kDarkGray;
-  final List<BoxShadow> _signinShadow = [
-    BoxShadow(
-      color: BrandColors.kHideGray,
-      offset: const Offset(5.0, 0.5),
-      blurRadius: 20.0,
-      spreadRadius: 1.0,
-      blurStyle: BlurStyle.normal,
-    )
-  ];
+  late Color _signinBackgroundColor;
+  late Color _signupBackgroundColor;
+  late Color _signinTextColor;
+  late Color _signupTextColor;
+
   // state bools
   bool _isSignin = true;
   bool _isSignup = false;
@@ -32,6 +25,14 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
   void initState() {
     _isSignin = true;
     _isSignup = false;
+    // sign in background and text color
+    _signinBackgroundColor = BrandColors.kColorDarkBlue;
+    _signinTextColor = BrandColors.kWhite;
+
+    // sign up background color
+    _signupBackgroundColor = BrandColors.kColorBackground;
+    _signupTextColor = BrandColors.kDarkGray;
+
     super.initState();
   }
 
@@ -45,145 +46,171 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
       body: Stack(
         children: <Widget>[
           const FancyPlasma(),
-          Center(
-            child: Container(
-              width: _width / 2,
-              margin: EdgeInsets.symmetric(
-                vertical: _height / 7.5,
-              ),
-              decoration: BoxDecoration(
-                color: BrandColors.kWhite,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      // sign in tab
-                      Expanded(
-                        child: InkWell(
-                          onTap: () {
-                            setState(() {
-                              _isSignin = true;
-                              _isSignup = false;
-                              _signinBackgroundColor = BrandColors.kColorDarkBlue;
-                              _signupBackgroundColor = BrandColors.kWhiteGray;
-                              _signinTextColor = BrandColors.kWhite;
-                              _signupTextColor = BrandColors.kDarkGray;
-                            });
-                          },
-                          child: MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            // on enter
-                            onEnter: (event) => setState(() {
-                              // change the background color
-                              _signinBackgroundColor = BrandColors.kBlue;
-                              _signinTextColor = BrandColors.kWhite;
-                            }),
+          Obx(
+            () => Center(
+              child: Container(
+                width: _width / 2,
+                margin: EdgeInsets.symmetric(
+                  vertical: _height / 10,
+                ),
+                decoration: BoxDecoration(
+                  color: themeController.isLightTheme() ? BrandColors.kColorBackground : BrandColors.kColorDarkTheme,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        // sign in tab
+                        Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              setState(() {
+                                _isSignin = true;
+                                _isSignup = false;
+                                _signinBackgroundColor = BrandColors.kColorDarkBlue;
+                                _signupBackgroundColor = BrandColors.kColorBackground;
+                                _signinTextColor = BrandColors.kWhite;
+                                _signupTextColor = BrandColors.kDarkGray;
+                              });
+                            },
+                            child: MouseRegion(
+                              cursor: SystemMouseCursors.click,
+                              // on enter
+                              onEnter: (event) => setState(() {
+                                // change the background color
+                                _signinBackgroundColor = BrandColors.kBlue;
+                                _signinTextColor = BrandColors.kWhite;
+                              }),
 
-                            // on exit
-                            onExit: (event) => setState(() {
-                              // change the background color
-                              _signinBackgroundColor = _isSignin ? BrandColors.kColorDarkBlue : BrandColors.kWhiteGray;
-                              _signinTextColor = _isSignin ? BrandColors.kWhite : BrandColors.kDarkGray;
-                            }),
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 10.0,
-                                horizontal: 10.0,
-                              ),
-                              decoration: BoxDecoration(
-                                color: _signinBackgroundColor,
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(10.0),
+                              // on exit
+                              onExit: (event) => setState(() {
+                                // change the background color
+                                _signinBackgroundColor =
+                                    _isSignin ? BrandColors.kColorDarkBlue : BrandColors.kColorBackground;
+                                _signinTextColor = _isSignin ? BrandColors.kWhite : BrandColors.kDarkGray;
+                              }),
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 10.0,
+                                  horizontal: 10.0,
                                 ),
-                                boxShadow: _isSignin ? _signinShadow : null,
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "Sign In".toUpperCase(),
-                                  style: GoogleFonts.roboto(
-                                    color: _signinTextColor,
-                                    fontSize: 22.0,
-                                    fontWeight: FontWeight.w700,
-                                    letterSpacing: 2.4,
+                                decoration: BoxDecoration(
+                                  color: _signinBackgroundColor,
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(10.0),
+                                  ),
+                                  boxShadow: (!themeController.isLightTheme() && _isSignin)
+                                      ? [
+                                          BoxShadow(
+                                            color:
+                                                themeController.isLightTheme() ? BrandColors.kHideGray : Colors.black12,
+                                            offset: const Offset(5.0, 0.5),
+                                            blurRadius: 10.0,
+                                            spreadRadius: 0.5,
+                                            blurStyle: BlurStyle.normal,
+                                          )
+                                        ]
+                                      : null,
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Sign In".toUpperCase(),
+                                    style: GoogleFonts.roboto(
+                                      color: _signinTextColor,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w700,
+                                      letterSpacing: 2.4,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
 
-                      // sign up tab
-                      Expanded(
-                        child: InkWell(
-                          onTap: () {
-                            setState(() {
-                              _isSignin = false;
-                              _isSignup = true;
-                              _signinBackgroundColor = BrandColors.kWhiteGray;
-                              _signupBackgroundColor = BrandColors.kColorDarkBlue;
-                              _signinTextColor = BrandColors.kDarkGray;
-                              _signupTextColor = BrandColors.kWhite;
-                            });
-                          },
-                          child: MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            // on enter
-                            onEnter: (event) => setState(() {
-                              // change the background color
-                              _signupBackgroundColor = BrandColors.kBlue;
-                              _signupTextColor = BrandColors.kWhite;
-                            }),
+                        // sign up tab
+                        Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              setState(() {
+                                _isSignin = false;
+                                _isSignup = true;
+                                _signinBackgroundColor = BrandColors.kColorBackground;
+                                _signupBackgroundColor = BrandColors.kColorDarkBlue;
+                                _signinTextColor = BrandColors.kDarkGray;
+                                _signupTextColor = BrandColors.kWhite;
+                              });
+                            },
+                            child: MouseRegion(
+                              cursor: SystemMouseCursors.click,
+                              // on enter
+                              onEnter: (event) => setState(() {
+                                // change the background color
+                                _signupBackgroundColor = BrandColors.kBlue;
+                                _signupTextColor = BrandColors.kWhite;
+                              }),
 
-                            // on exit
-                            onExit: (event) => setState(() {
-                              // change the background color
-                              _signupBackgroundColor = _isSignup ? BrandColors.kColorDarkBlue : BrandColors.kWhiteGray;
-                              _signupTextColor = _isSignup ? BrandColors.kWhite : BrandColors.kDarkGray;
-                            }),
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 10.0,
-                                horizontal: 10.0,
-                              ),
-                              decoration: BoxDecoration(
-                                color: _signupBackgroundColor,
-                                borderRadius: const BorderRadius.only(
-                                  topRight: Radius.circular(10.0),
+                              // on exit
+                              onExit: (event) => setState(() {
+                                // change the background color
+                                _signupBackgroundColor =
+                                    _isSignup ? BrandColors.kColorDarkBlue : BrandColors.kWhiteGray;
+                                _signupTextColor = _isSignup ? BrandColors.kWhite : BrandColors.kDarkGray;
+                              }),
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 10.0,
+                                  horizontal: 10.0,
                                 ),
-                                boxShadow: _isSignup ? _signinShadow : null,
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "Sign Up".toUpperCase(),
-                                  style: GoogleFonts.roboto(
-                                    color: _signupTextColor,
-                                    fontSize: 22.0,
-                                    fontWeight: FontWeight.w700,
-                                    letterSpacing: 2.4,
+                                decoration: BoxDecoration(
+                                  color: _signupBackgroundColor,
+                                  borderRadius: const BorderRadius.only(
+                                    topRight: Radius.circular(10.0),
+                                  ),
+                                  boxShadow: (!themeController.isLightTheme() && _isSignup)
+                                      ? [
+                                          BoxShadow(
+                                            color:
+                                                themeController.isLightTheme() ? BrandColors.kHideGray : Colors.black12,
+                                            offset: const Offset(5.0, 0.5),
+                                            blurRadius: 10.0,
+                                            spreadRadius: 0.5,
+                                            blurStyle: BlurStyle.normal,
+                                          )
+                                        ]
+                                      : null,
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Sign Up".toUpperCase(),
+                                    style: GoogleFonts.roboto(
+                                      color: _signupTextColor,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w700,
+                                      letterSpacing: 2.4,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    // add some animation to the sign in and sign up forms
-                    child: AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 500),
-                      // switchInCurve: Curves.easeIn,
-                      // switchOutCurve: Curves.easeOut,
-                      child: _isSignin ? const LoginForm() : const RegisterForm(),
+                      ],
                     ),
-                  ),
-                ],
+                    Expanded(
+                      // add some animation to the sign in and sign up forms
+                      child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 500),
+                        // switchInCurve: Curves.easeIn,
+                        // switchOutCurve: Curves.easeOut,
+                        child: _isSignin ? const LoginForm() : const RegisterForm(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/pages/auth/register/register_form.dart
+++ lib/pages/auth/register/register_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dynasty_urban_style/index.dart';
+import 'package:get/get.dart';
 
 class RegisterForm extends StatefulWidget {
   const RegisterForm({Key? key}) : super(key: key);
@@ -15,6 +16,9 @@ class _RegisterFormState extends State<RegisterForm> {
   // define controllers for password and email address
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
 
   // password visibility
   bool _passwordVisible = false;
@@ -34,94 +38,117 @@ class _RegisterFormState extends State<RegisterForm> {
     // width and height of the screen
     double _width = MediaQuery.of(context).size.width;
 
-    return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: 10.0,
-        horizontal: _width * 0.1,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 15.0),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 10.0,
-              horizontal: 10.0,
-            ),
-            decoration: const BoxDecoration(),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  // add a text field for email address
-                  CustomFormField(
-                    emailController: _emailController,
-                    label: 'Email Address',
-                    prefixIcon: Icon(
-                      Icons.email_outlined,
-                      color: BrandColors.kColorDarkBlue,
-                      size: 26.0,
-                    ),
-                  ),
-                  const SizedBox(height: 18.0),
-                  // add a text field for first name
-                  CustomFormField(
-                    emailController: _emailController,
-                    label: 'First Name',
-                    prefixIcon: Icon(
-                      Icons.person_outline,
-                      color: BrandColors.kColorDarkBlue,
-                      size: 26.0,
-                    ),
-                  ),
-                  const SizedBox(height: 18.0),
-                  // add a text field for lastname name
-                  CustomFormField(
-                    emailController: _emailController,
-                    label: 'First Name',
-                    prefixIcon: Icon(
-                      Icons.person_outline,
-                      color: BrandColors.kColorDarkBlue,
-                      size: 26.0,
-                    ),
-                  ),
-                  const SizedBox(height: 18.0),
-                  // add a text field for password
-                  CustomFormField(
-                    emailController: _passwordController,
-                    label: "Password",
-                    isObscured: !_passwordVisible,
-                    prefixIcon: Icon(
-                      Icons.lock_outline,
-                      color: BrandColors.kColorDarkBlue,
-                      size: 26.0,
-                    ),
-                    suffixIcon: InkWell(
-                      onTap: () {
-                        setState(() {
-                          _passwordVisible = !_passwordVisible;
-                        });
-                      },
-                      child: Icon(
-                        _passwordVisible ? Icons.visibility_outlined : Icons.visibility_off_outlined,
-                        color: BrandColors.kLightBlueSky,
+    return Obx(
+      () => Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: _width * 0.1,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 15.0),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 10.0,
+              ),
+              decoration: const BoxDecoration(),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    // add a text field for email address
+                    CustomFormField(
+                      emailController: _emailController,
+                      label: 'Email Address',
+                      focusedBorderColor:
+                          themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kColorDarkGreen,
+                      prefixIcon: Icon(
+                        Icons.email_outlined,
+                        color: themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kLightBlueSky,
+                        size: 26.0,
                       ),
                     ),
-                  ),
+                    const SizedBox(height: 18.0),
+                    // add a text field for first name
+                    CustomFormField(
+                      emailController: _firstNameController,
+                      label: 'First Name',
+                      focusedBorderColor:
+                          themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kColorDarkGreen,
+                      prefixIcon: Icon(
+                        Icons.person_outline,
+                        color: themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kLightBlueSky,
+                        size: 26.0,
+                      ),
+                    ),
+                    const SizedBox(height: 18.0),
+                    // add a text field for lastname name
+                    CustomFormField(
+                      emailController: _lastNameController,
+                      label: 'Last Name',
+                      focusedBorderColor:
+                          themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kColorDarkGreen,
+                      prefixIcon: Icon(
+                        Icons.person_outline,
+                        color: themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kLightBlueSky,
+                        size: 26.0,
+                      ),
+                    ),
+                    const SizedBox(height: 18.0),
+                    // add a text field for phone number
+                    CustomFormField(
+                      emailController: _phoneNumberController,
+                      label: 'Phone Number',
+                      focusedBorderColor:
+                          themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kColorDarkGreen,
+                      prefixIcon: Icon(
+                        Icons.call_outlined,
+                        color: themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kLightBlueSky,
+                        size: 26.0,
+                      ),
+                    ),
+                    const SizedBox(height: 18.0),
+                    // add a text field for password
+                    CustomFormField(
+                      emailController: _passwordController,
+                      label: "Password",
+                      isObscured: !_passwordVisible,
+                      focusedBorderColor:
+                          themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kColorDarkGreen,
+                      prefixIcon: Icon(
+                        Icons.lock_outline,
+                        color: themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kLightBlueSky,
+                        size: 26.0,
+                      ),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                        child: Icon(
+                          _passwordVisible ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+                          color: BrandColors.kLightBlueSky,
+                        ),
+                      ),
+                    ),
 
-                  const SizedBox(height: 22.0),
-                  // add a button for logging in
-                  Custombutton(
-                    text: "Register",
-                    color: BrandColors.kColorDarkBlue,
-                    onPressed: () {},
-                  ),
-                ],
+                    const SizedBox(height: 22.0),
+                    // add a button for logging in
+                    Custombutton(
+                      text: "Register",
+                      color: themeController.isLightTheme() ? BrandColors.kColorDarkBlue : BrandColors.kColorLightBlue,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
